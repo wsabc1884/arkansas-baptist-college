d1f3cb7d114139fce7292e873bbeb57b118888b7
Jones Jr, Roger <jon3sx@gmail.com>
2026-02-26 00:13:47 -0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon.ico',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon.ico',
         media: '(prefers-color-scheme: dark)',
       },
       {
